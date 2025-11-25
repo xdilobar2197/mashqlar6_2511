ac8b91d6db94124c76f162566660f0500c4b53d1
xdilobar2197 <xdilobar2197@gmail.com>
2025-11-25 17:55:29 +0500

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# mashqlar6_2511
+def faktarial(son):
+    if son < 0:
+        return 0
+    fakt = 1
+    for i in range(1, son + 1):
+        fakt *= 1
+        return fakt
+print(faktarial(5))
+print(faktarial(-7))
